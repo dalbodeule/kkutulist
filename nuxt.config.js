7a01306a30c6030ef79192e578eeac70163f8c88
small_sunshine <jioo0224@naver.com>
2017-12-04 21:19:29 +0900

    초기 커밋

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack')
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { 
+	height: '0.5em'
+  },
+  router: {
+	middleware: ['ssr-cookie']
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    },
+    vendor: [
+      'jquery'
+    ],
+    plugins: [
+      new webpack.ProvidePlugin({
+        jQuery: 'jquery',
+        $: 'jquery',
+        jquery: 'jquery',
+        'window.jQuery': 'jquery'
+      })
+    ]
+  },
+  modules: [
+    ['@nuxtjs/google-adsense'],
+    ['@nuxtjs/google-analytics'],
+    ['@nuxtjs/sitemap']
+  ],
+  'google-adsense': {
+    id: 'ca-pub-2810659463174293',
+    pageLevelAds: true,
+    analyticsUacct: 'UA-61070671-',
+    analyticsDomainName: 'mori.space'
+  },
+  'google-analytics': {
+    id: 'UA-61070671-'
+  },
+  sitemap: {
+    path: '/sitemap.xml'
+  }
+}
